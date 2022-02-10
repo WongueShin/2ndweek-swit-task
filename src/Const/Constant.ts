c9cf5a64c8f1ac19aef7b590989096d0528d977e
WongueShin <won7963kr@gmail.com>
2022-02-10 21:59:35 +0900

    Add: Constant.ts profile3 경로를 추가하였습니다.

--- src/Const/Constant.ts
+++ src/Const/Constant.ts
@@ -1,7 +1,7 @@
 export enum USER_PROFILE_PATH {
   ID_926931d2 = './Images/profile1.png',
   ID_c58e261b = './Images/profile2.png',
-  ID_65bd3353 = 'USER',
+  ID_65bd3353 = './Images/profile3.png',
 }
 
 export const USER: string = 'USER';
