ede20f1d276d812f623006db6f2f2c4cd72108d2
SEOYUN KIM <nado0903@gmail.com>
2022-02-11 00:18:41 +0900

    Feat: 로그인 버튼 구현

--- src/index.tsx
+++ src/index.tsx
@@ -1,10 +1,24 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
+import reportWebVitals from './reportWebVitals';
+import { Provider } from 'react-redux';
+import { createStore, Store } from 'redux';
+import { persistStore } from 'redux-persist';
+import { PersistGate } from 'redux-persist/integration/react';
+import rootReducer from './Redux/index.Redux';
 
+const store: Store<any, any> = createStore(rootReducer);
+const persistor = persistStore(store);
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+  <Provider store={store}>
+    <PersistGate loading={null} persistor={persistor}>
+      <React.StrictMode>
+        <App />
+      </React.StrictMode>
+    </PersistGate>
+  </Provider>,
   document.getElementById('root')
 );
+
+reportWebVitals();
