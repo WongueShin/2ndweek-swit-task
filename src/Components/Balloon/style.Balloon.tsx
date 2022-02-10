0d8a78d97282dcbdba94ba68c5e2f5629d7ea4e0
zerochae <zerochae@kakao.com>
2022-02-10 23:58:46 +0900

    Add: 채팅 대화목록 구현

--- src/Components/Balloon/style.Balloon.tsx
+++ src/Components/Balloon/style.Balloon.tsx
@@ -7,23 +7,23 @@ export const Container = styled.div<{ background: string }>`
   display: flex;
   width: 200px;
   height: fit-content;
-  background: ${(props) => (props.background === "" ? "#fff" : "#5c5cfb")};
+  background: ${(props) => (props.background === "사람3" ? "#fff" : "#5c5cfb")};
   position: relative;
-  color: ${(props) => (props.background === "" ? "#000" : "#fff")};
+  color: ${(props) => (props.background === "사람3" ? "#000" : "#fff")};
   padding: 12px;
   margin-left: 20px;
   border-radius: 8px;
   word-break: break-all;
 
   &:after {
-    border-top: 6px solid transparent;
+    border-top: 2px solid transparent;
     border-left: 10px solid transparent;
     border-right: 10px solid
-      ${(props) => (props.background === "" ? "#fff" : "#5c5cfb")};
-    border-bottom: 6px solid transparent;
+      ${(props) => (props.background === "사람3" ? "#fff" : "#5c5cfb")};
+    border-bottom: 10px solid transparent;
     content: "";
     position: absolute;
-    top: 10px;
+    top: 8px;
     left: -18px;
   }
 `;
--- src/Components/Chat/index.Chat.tsx
+++ src/Components/Chat/index.Chat.tsx
@@ -2,13 +2,16 @@ import React from "react";
 import Balloon from "Components/Balloon/index.Balloon";
 import UserProfile from "Components/UserProfile/index.UserProfile";
 import * as S from "Components/Chat/style.Chat";
+import { dataType } from 'src/types/dataType';
 
-const Chat = ():JSX.Element => {
+
+const Chat = ({data} :any) :JSX.Element => {
+
     return(
         <S.ChatWarpper>
-            <UserProfile/>
+            <UserProfile USER_PROFLE_PATH={data.profileImage}/>
             <S.BalloonWarpper>
-                <Balloon/>
+                <Balloon content={data.content} background={data.userName}/>
             </S.BalloonWarpper>
         </S.ChatWarpper>
     )
