565ce6c679955384587ec5d56bf5d59bd51ebcd6
WongueShin <won7963kr@gmail.com>
2022-02-10 21:26:55 +0900

    Add: UserProfile 동적으로 생성될 유저프로필을 작성하였습니다

---
+++ src/Components/UserProfile/style.UserProfile.ts
@@ -0,0 +1,16 @@
+import styled from "styled-components";
+
+export const ProfileWarrper = styled.div`
+    width: 50px;
+    height: 50px;
+    border-radius: 50%;
+    background-color: white;
+    border: 2px solid #A8A8A8;
+`
+
+export const ProfileImg = styled.div<{ProFilePath: string}>`
+    height: 100%;
+    background-image: url(${Props=> Props.ProFilePath});
+    background-repeat: no-repeat;
+    background-size: cover;
+`
