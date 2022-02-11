99a60f7cedff3f45cdfa9e3e9e07f1290fb54bc4
WongueShin <won7963kr@gmail.com>
2022-02-11 13:51:12 +0900

    Style: Input/

--- src/Components/Input/style.input.ts
+++ src/Components/Input/style.input.ts
@@ -5,6 +5,10 @@ export const InputContainer = styled.div`
     position: relative;
     left: 0;
     bottom: 0;
+    padding: 10px;
+    display: flex;
+    box-sizing: border-box;
+    flex-direction: column;
 `
 
 export const MessageTextarea = styled.textarea`
