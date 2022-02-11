ae3ab2ef444ce68cffb284b644588df869a8afad
SEOYUN KIM <nado0903@gmail.com>
2022-02-11 22:32:48 +0900

    Style: Login 모달창 수정

--- src/Components/Login/index.Login.tsx
+++ src/Components/Login/index.Login.tsx
@@ -1,4 +1,4 @@
-import React, { useState, ChangeEvent } from 'react';
+import React, { useState, ChangeEvent, useEffect } from 'react';
 import { useSelector, useDispatch } from 'react-redux';
 import { RootState } from '../../Redux/index.Redux';
 import { onLogin, onLogout } from '../../Redux/user.Redux';
@@ -27,27 +27,34 @@ const Login = () => {
 
   return (
     <>
-      <S.LoginContainer>
-        {user.isLogin === false ? (
-          <>
-            <S.LoginForm onSubmit={handleOnSubmit}>
-              <S.NameInput
-                name="userNameInput"
-                value={userNameInput}
-                onChange={onChangeUserName}
-                placeholder="이름을 입력하세요"
-              />
-              <S.LoginLogoutButton type="submit">로그인</S.LoginLogoutButton>
-            </S.LoginForm>
-          </>
-        ) : (
-          <>
+      {user.isLogin === false ? (
+        <>
+          <S.LoginBackground>
+            <S.LoginContainer>
+              <S.LoginInfoSpan>
+                로그인 후 대화에 참여할 수 있습니다.
+              </S.LoginInfoSpan>
+              <S.LoginForm onSubmit={handleOnSubmit}>
+                <S.NameInput
+                  name="userNameInput"
+                  value={userNameInput}
+                  onChange={onChangeUserName}
+                  placeholder="이름을 입력하세요"
+                />
+                <S.LoginLogoutButton type="submit">로그인</S.LoginLogoutButton>
+              </S.LoginForm>
+            </S.LoginContainer>
+          </S.LoginBackground>
+        </>
+      ) : (
+        <>
+          <S.LogoutContainer>
             <S.LoginLogoutButton onClick={onLogoutClick}>
               로그아웃
             </S.LoginLogoutButton>
-          </>
-        )}
-      </S.LoginContainer>
+          </S.LogoutContainer>
+        </>
+      )}
     </>
   );
 };
