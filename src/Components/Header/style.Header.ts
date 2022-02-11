9fa0b34656c34c08047ae6acea9ebbdc7c80843e
SEOYUN KIM <nado0903@gmail.com>
2022-02-11 21:07:06 +0900

    Modify: Header 스타일 수장

--- src/Components/Header/style.Header.ts
+++ src/Components/Header/style.Header.ts
@@ -2,8 +2,11 @@ import styled from 'styled-components';
 
 export const HeaderContainer = styled.div`
   display: flex;
-  margin: 26px 0px;
+  justify-content: space-between;
+  padding: 26px 0px;
   width: 500px;
+  box-shadow: inset 0 -7px 9px -7px rgba(0, 0, 0, 0.1);
+  //box-shadow: inset 0 7px 9px -7px rgba(0, 0, 0, 0.1);
 `;
 
 export const ChatName = styled.span`
@@ -12,14 +15,26 @@ export const ChatName = styled.span`
   line-height: 100%;
   font-weight: 500;
   vertical-align: bottom;
+  margin-left: 14px;
 `;
 export const ChatUserCount = styled.span`
   display: inline-block;
   font-size: 16px;
   line-height: 100%;
   vertical-align: bottom;
-  padding-top: 5px;
+  padding-bottom: 3px;
   color: gray;
 `;
 
-export const HeaderContent = styled.div``;
+export const HeaderTitle = styled.div``;
+export const HeaderSettingBtn = styled.button`
+  width: 30px;
+  height: 30px;
+  border-radius: 15px;
+  margin-right: 16px;
+  background: #f5f5f5;
+  border: none;
+  :hover {
+    background-color: #f0eded;
+  }
+`;
