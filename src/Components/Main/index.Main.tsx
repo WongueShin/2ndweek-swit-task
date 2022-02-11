154d4c2b0e93ae88ba0f3c0a27811e29b9c04868
SEOYUN KIM <nado0903@gmail.com>
2022-02-11 10:24:24 +0900

    Feat: 로그인, 로그아웃 액션 data state 연동

--- src/Components/Main/index.Main.tsx
+++ src/Components/Main/index.Main.tsx
@@ -1,8 +1,10 @@
-import Messenger from "Components/Messenger/index.Messenger";
+import Login from 'Components/Login/index.Login';
+import Messenger from 'Components/Messenger/index.Messenger';
 
 const Main = (): JSX.Element => {
   return (
     <>
+      <Login />
       <Messenger />
     </>
   );
--- src/Components/Messenger/index.Messenger.tsx
+++ src/Components/Messenger/index.Messenger.tsx
@@ -1,18 +1,42 @@
-import * as S from "Components/Messenger/style.Messenger";
-import React, { useState } from "react";
-import Chat from "Components/Chat/index.Chat";
-import Input from "Components/Input/index.Input";
-import { messageJson } from "Components/Messenger/data";
-import { dataType } from "src/types/dataType";
+import * as S from 'Components/Messenger/style.Messenger';
+import React, { useEffect, useState } from 'react';
+import Chat from 'Components/Chat/index.Chat';
+import { useSelector } from 'react-redux';
+import { messageJson } from 'Components/Messenger/data';
+import { dataType } from 'src/types/dataType';
+import { RootState } from 'src/Redux/index.Redux';
+import Input from 'Components/Input/index.Input';
 
 const Messenger = (): JSX.Element => {
-  const [data, setData] = useState<dataType[]>(messageJson);
-  console.log(data);
+  const user = useSelector((state: RootState) => state.user);
+  let messageJsonCopy = JSON.parse(JSON.stringify(messageJson));
+  const [data, setData] = useState<dataType[]>(messageJsonCopy);
+
+  useEffect(() => {
+    let tmpData: dataType[] = [...data];
+    if (user.userId) {
+      console.log('로그인');
+      tmpData.map(el => {
+        if (el.userId === user.userId) {
+          console.log('gd');
+          return (el.userName = user.userName);
+        } else return null;
+      });
+      setData(tmpData);
+    } else {
+      console.log('로그아웃');
+      let messageJsonReset = JSON.parse(JSON.stringify(messageJson));
+      setData(messageJsonReset);
+    }
+    console.log(messageJson);
+    console.log(tmpData);
+    console.log(data);
+  }, [user.userName, user.userId]);
   return (
     <>
       <S.MessengerContainer>
         {data &&
-          data.map((el) => {
+          data.map(el => {
             return (
               <S.Message>
                 <Chat data={el} />
@@ -20,7 +44,7 @@ const Messenger = (): JSX.Element => {
             );
           })}
       </S.MessengerContainer>
-      <Input/>
+      <Input />
     </>
   );
 };
