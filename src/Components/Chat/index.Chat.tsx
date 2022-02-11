59f8ebb95dd6784ebdf82386472268b826900f8c
SEOYUN KIM <nado0903@gmail.com>
2022-02-11 10:59:22 +0900

    Modify: Chat user state연동

--- src/Components/Chat/index.Chat.tsx
+++ src/Components/Chat/index.Chat.tsx
@@ -1,20 +1,23 @@
-import React from "react";
-import Balloon from "Components/Balloon/index.Balloon";
-import UserProfile from "Components/UserProfile/index.UserProfile";
-import Button from "Components/Button/index.Button";
-import * as S from "Components/Chat/style.Chat";
-import { dataType } from "src/types/dataType";
+import React from 'react';
+import Balloon from 'Components/Balloon/index.Balloon';
+import UserProfile from 'Components/UserProfile/index.UserProfile';
+import Button from 'Components/Button/index.Button';
+import * as S from 'Components/Chat/style.Chat';
+import { dataType } from 'src/types/dataType';
+import { useSelector } from 'react-redux';
+import { RootState } from 'src/Redux/index.Redux';
 
 interface ChatPropsType {
   data: dataType;
 }
 
 const Chat = ({ data }: ChatPropsType): JSX.Element => {
+  const user = useSelector((state: RootState) => state.user);
   return (
     <S.ChatWarpper>
-      {data.userName === "사람3" ? (
+      {data.userId === user.userId ? (
         <>
-          <Button userName={data.userName}/>
+          <Button userName={data.userName} />
           <S.BalloonWarpper>
             <Balloon content={data.content} userName={data.userName} />
           </S.BalloonWarpper>
@@ -22,12 +25,12 @@ const Chat = ({ data }: ChatPropsType): JSX.Element => {
         </>
       ) : (
         <>
-          {" "}
+          {' '}
           <UserProfile USER_PROFLE_PATH={data.profileImage} />
           <S.BalloonWarpper>
             <Balloon content={data.content} userName={data.userName} />
           </S.BalloonWarpper>
-          <Button userName={data.userName}/>
+          <Button userName={data.userName} />
         </>
       )}
     </S.ChatWarpper>
