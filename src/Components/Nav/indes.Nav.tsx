1db50b332d9ba3a3f548bee4fe5578c3f4bc8851
WongueShin <won7963kr@gmail.com>
2022-02-12 12:39:03 +0900

    Add:Nav

---
+++ src/Components/Nav/indes.Nav.tsx
@@ -0,0 +1,40 @@
+import React from "react";
+import * as S from "Components/Nav/style.Nav";
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faTableColumns, faUser, faBookOpen, faBarsStaggered, faEnvelope, faGear } from '@fortawesome/free-solid-svg-icons';
+
+const handleOnClick = ():void => {
+    alert("데모버전에서는 지원되지 않는 기능입니다.")
+}
+
+const Nav = ():JSX.Element => {
+    return(
+        <S.NavContainer>
+            <S.LogoContainer>
+                <S.LogoDiv/>
+            </S.LogoContainer>
+            <S.IconList>
+                <S.IconContainer onClick={handleOnClick}>
+                    <FontAwesomeIcon icon={faTableColumns}/>
+                </S.IconContainer>
+                <S.IconContainer onClick={handleOnClick}>
+                    <FontAwesomeIcon icon={faUser}/>
+                </S.IconContainer>
+                <S.IconContainer onClick={handleOnClick}>
+                    <FontAwesomeIcon icon={faBookOpen}/>
+                </S.IconContainer>
+                <S.IconContainer onClick={handleOnClick}>
+                    <FontAwesomeIcon icon={faBarsStaggered}/>
+                </S.IconContainer>
+                <S.SelectedContainer>
+                    <FontAwesomeIcon icon={faEnvelope}/>
+                </S.SelectedContainer>
+                <S.IconContainer onClick={handleOnClick}>
+                    <FontAwesomeIcon icon={faGear}/>
+                </S.IconContainer>
+            </S.IconList>
+        </S.NavContainer>
+    )
+}
+
+export default Nav;
